2477e7cb091ee53ba34548e5478efba3fc01fe94
zhongqin0820 <zhongqin0820@163.com>
2018-01-12 19:30:53 +0800

    modify data path

--- SupervisedLearning/TensorMnist.py
+++ SupervisedLearning/TensorMnist.py
@@ -2,7 +2,7 @@ import numpy as np
 import tensorflow as tf
 
 from tensorflow.examples.tutorials.mnist import input_data
-mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
+mnist = input_data.read_data_sets("../MNIST_data/", one_hot=True)
 print (mnist.train.labels[1000])
 
 x = tf.placeholder(tf.float32, [None, 784])
