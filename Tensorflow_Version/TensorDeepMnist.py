166b885eb13ffbb7bd1aa7d103e03e52c5088770
zhongqin0820 <zhongqin0820@163.com>
2018-01-13 15:20:26 +0800

    update fashion-MNIST train result

--- Tensorflow_Version/TensorDeepMnist.py
+++ Tensorflow_Version/TensorDeepMnist.py
@@ -2,7 +2,7 @@
 import tensorflow as tf
 
 from tensorflow.examples.tutorials.mnist import input_data
-mnist = input_data.read_data_sets("../MNIST_data/", one_hot=True)
+mnist = input_data.read_data_sets("../fashion-mnist/", one_hot=True)
 #print (mnist.train.labels[1000])
 
 sess = tf.InteractiveSession()
@@ -67,6 +67,7 @@ train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
 correct_prediction = tf.equal(tf.argmax(y_conv, 1), tf.argmax(y_, 1))
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
+file_handle = open('fashion_result.txt','w+')
 with tf.Session() as sess:
   sess.run(tf.global_variables_initializer())
   for i in range(20000):
@@ -75,7 +76,9 @@ with tf.Session() as sess:
       train_accuracy = accuracy.eval(feed_dict={
           x: batch[0], y_: batch[1], keep_prob: 1.0})
       print('step %d, training accuracy %g' % (i, train_accuracy))
+      file_handle.write(','.join([str(i),str(train_accuracy)]))
     train_step.run(feed_dict={x: batch[0], y_: batch[1], keep_prob: 0.5})
 
   print('test accuracy %g' % accuracy.eval(feed_dict={
       x: mnist.test.images, y_: mnist.test.labels, keep_prob: 1.0}))
+file_handle.close()
--- draw.py
+++ draw.py
@@ -2,6 +2,11 @@ import matplotlib.pyplot as plt
 from matplotlib.ticker import MultipleLocator
 
 def loadfile(filename):
+    """
+    load training data file
+    :param filename: resolute path correspond to this file
+    :return: x: itration, y: train accurrency
+    """
     data = open(filename,'r')
     x = []
     y = []
@@ -13,26 +18,48 @@ def loadfile(filename):
     return x, y
 
 
-def pltdata(x, y):
-    fig = plt.figure(1)
-    ax = fig.subplots(1)
-    ax.plot(x,y)
+def pltdata(x, y, x_f, y_f):
+    """
+    use matplotlib.pylib to visualize data
+    :param x: take care that x equals to x_f here because they are using same itrations 
+    :param y: 
+    :param x_f: 
+    :param y_f: 
+    :return: 
+    """
+    fig = plt.figure(1,figsize=(9,6))
+    # tick margin
     xmajorLocator = MultipleLocator(4000)
     xminorLocator = MultipleLocator(100)
     ymajorLocator = MultipleLocator(0.2)
     yminorLocator = MultipleLocator(0.05)
+    # left y axis
+    ax = fig.subplots(1)
+    ax.plot(x,y,color="blue",linestyle="-",label="MNIST")
+    ax.legend(loc="lower left",shadow=True)
+    ax.set_ylabel('MNIST-Accurrency')
     ax.xaxis.set_major_locator(xmajorLocator)
     ax.xaxis.set_minor_locator(xminorLocator)
     ax.yaxis.set_major_locator(ymajorLocator)
     ax.yaxis.set_minor_locator(yminorLocator)
     ax.xaxis.grid(True, which='major')
     ax.yaxis.grid(True, which='minor')
-    plt.xlabel('Iterations/100')
-    plt.ylabel('Accurrency')
-    plt.title('Supervised Learning:Traing MNIST')
-    plt.savefig('deep_mnist.png',dpi=100)
+    # right y axis
+    ax_f = ax.twinx()
+    ax_f.plot(x_f, y_f, color="red",linestyle="-",label="fashion-MNIST")
+    ax_f.legend(loc="lower right",shadow=True)
+    ax_f.set_ylabel("fashion-MNIST-Accurrency")
+    ax_f.xaxis.set_major_locator(xmajorLocator)
+    ax_f.xaxis.set_minor_locator(xminorLocator)
+    ax_f.yaxis.set_major_locator(ymajorLocator)
+    ax_f.yaxis.set_minor_locator(yminorLocator)
+    # common x axis display
+    ax.set_xlabel('Iterations/100')
+    plt.title('Supervised Learning:Traing Result')
+    plt.savefig('result.png',dpi=100)
     plt.show()
 
 if __name__ == "__main__":
+    x_f, y_f = loadfile("Misc/fashion_result.txt")
     x, y = loadfile("Misc/deepResult.txt")
-    pltdata(x,y)
+    pltdata(x,y,x_f,y_f)
